606da3e2ca44898488668e83ef7b99e283bb48d9
Arthur Lecomte <arthurlecomtefr@gmail.com>
2023-05-16 10:16:52 +0200

    MAJ gh-pages

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const app = require ('express')();
+const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+
+      
 app.get('/', (req,res) => 
 {
 	res.sendFile(`${__dirname}/index.html`)
